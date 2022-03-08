c75398de2c11e4edeb7e708f2243c810c9ea826a
sabbirahmed07 <sabbirsristy@gmail.com>
2022-03-08 20:59:51 +0600

    mongo key added in github secrets

--- api/dist/config/db.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.DB_CONFIG = void 0;
-exports.DB_CONFIG = {
-    url: 'mongodb+srv://Sabbir:iutcse36@clubmanagement.6ugdq.mongodb.net/clubManagement',
-};
